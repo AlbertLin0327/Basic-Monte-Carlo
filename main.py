96ac2f84a35f81af749c04032470853d5b4e5b51
AlbertLin0327 <linhsinkai@gmail.com>
2021-12-08 17:26:36 +0800

    coding style improve

--- main.py
+++ main.py
@@ -4,33 +4,37 @@ from MonteCarlo import MonteCarlo
 # main function
 def main():
 
-	# take in the input from user
-	print('Enter: spot price, time interval, interest(%), dividend(%), volatility(%), period, stimulations')
+    # take in the input from user
+    print(
+        "Enter: spot price, time interval, interest(%), dividend(%), volatility(%), period, stimulations"
+    )
 
-	while True:
-		try:
-			spot, time, interest, dividend, volatility, period, simulations = map(float, input().split())
-			break
+    while True:
+        try:
+            spot, time, interest, dividend, volatility, period, simulations = map(
+                float, input().split()
+            )
+            break
 
-		except:
-			print("WRONG FORMAT!!!")
+        except:
+            print("WRONG FORMAT!!!")
 
-	Model = MonteCarlo(spot, time, interest, dividend, volatility, period, simulations)
+    Model = MonteCarlo(spot, time, interest, dividend, volatility, period, simulations)
 
-	# Calculating Price
-	Model.random_path()
-	Model.pricing()
+    # Calculating Price
+    Model.random_path()
+    Model.pricing()
 
-	print('')
-	print('-----------------------------')
-	print(f'Price: {Model.mean}')
-	print(f'Std: {Model.std}')
-	print('-----------------------------')
-	print('')
+    print("")
+    print("-----------------------------")
+    print(f"Price: {Model.mean}")
+    print(f"Std: {Model.std}")
+    print("-----------------------------")
+    print("")
 
-	# Plot distribution
-	Model.plot()
-	
+    # Plot distribution
+    Model.plot()
 
-if __name__ == '__main__':
-	main()
+
+if __name__ == "__main__":
+    main()
